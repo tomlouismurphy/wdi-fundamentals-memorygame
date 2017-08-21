90aeb6e73a5d4be44876741fbe72510a1a14e722
Jamie Mermelstein <Jamie@Jamies-MacBook-Air-5.local>
2017-08-21 16:58:12 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardFour)
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+if (cardsInPlay[0] === "queen") {
+	console.log("User flipped queen");
+} else if (cardsInPlay[0] === "king") {
+	console.log("User flipped king");
+}
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+if (cardsInPlay[1] === "king") {
+	console.log("User flipped king");
+} else if (cardsInPlay[1] === "queen") {
+	console.log("User flipped queen");
+}
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!")
+} else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
+	alert("Sorry, try again.")
+}
