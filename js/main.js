27e807991e8d04f9c6f43ec6d545514af7a41aa0
Jamie Mermelstein <Jamie@Jamies-MacBook-Air-5.local>
2017-08-23 12:54:54 -0500

    created game logic with functions

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-if (cardsInPlay[0] === "queen") {
-	console.log("User flipped queen");
-} else if (cardsInPlay[0] === "king") {
-	console.log("User flipped king");
+
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
+		alert("Sorry, try again.");
+	}
 }
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-if (cardsInPlay[1] === "king") {
-	console.log("User flipped king");
-} else if (cardsInPlay[1] === "queen") {
-	console.log("User flipped queen");
-}
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!")
-} else if (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1]) {
-	alert("Sorry, try again.")
-}
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
